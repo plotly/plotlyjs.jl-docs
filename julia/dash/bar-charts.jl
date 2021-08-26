b9e7eb332b3eec16c40a4f2268e66c3673a3dc7c
Trevor Lyon <trevor.lyon@icloud.com>
2021-08-26 15:43:08 -0300

    bar charts dash example

---
+++ julia/dash/bar-charts.jl
@@ -0,0 +1,25 @@
+using Dash
+using DashHtmlComponents
+using DashCoreComponents
+using PlotlyJS, CSV, DataFrames
+
+app = dash(external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"])
+
+df = dataset(DataFrame, "tips")
+days = unique(df.day)
+
+app.layout = html_div() do
+    dcc_dropdown(id="dropdown", options=[Dict("label"=>x, "value"=>x) for x in days]),
+    dcc_graph(
+        id = "barchart"
+    )
+end
+
+callback!(app, Output("barchart", "figure"), Input("dropdown", "value")) do val
+   mask = df[df.day .== val, :]
+   fig = plot(mask, kind="bar", x=:sex, y=:total_bill, color=:smoker, barmode="group")
+   return fig
+end
+
+
+run_server(app, "0.0.0.0", 8080)
