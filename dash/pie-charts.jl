20f3fd82cdd466ab7305d29083a6ba784565d7cf
Trevor Lyon <trevor.lyon@icloud.com>
2021-08-27 15:13:57 -0300

    pie-charts

---
+++ dash/pie-charts.jl
@@ -0,0 +1,34 @@
+using Dash
+using DashHtmlComponents
+using DashCoreComponents
+using PlotlyJS, CSV, DataFrames
+
+app = dash(external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"])
+
+df = dataset(DataFrame, "tips")
+
+app.layout = html_div() do
+    html_p("Names:"),
+    dcc_dropdown(
+        id="names", 
+        value="day", 
+        options=[Dict("value"=> x, "label"=> x)
+                 for x in ["smoker", "day", "time", "sex"]],
+        clearable=false
+    ),
+    html_p("Values:"),
+    dcc_dropdown(
+        id="values", 
+        value="total_bill", 
+        options=[Dict("value"=> x, "label"=> x)
+                 for x in ["total_bill", "tip", "size"]],
+        clearable=false
+    ),
+    dcc_graph(id="pie-chart")
+end
+
+callback!(app, Output("pie-chart", "figure"), [Input("values", "value"), Input("names", "value")]) do v, n
+    fig = plot(pie(values=df[!,v], labels=df[!, n]))
+    return fig
+end
+run_server(app, "0.0.0.0", 8080)
