773a6ad57f38deb541ec876c2e7745359c5e1d8d
Trevor Lyon <trevor.lyon@icloud.com>
2021-08-27 15:27:01 -0300

    DASH box-plots

---
+++ dash/box-plots.jl
@@ -0,0 +1,39 @@
+using Dash
+using DashHtmlComponents
+using DashCoreComponents
+using PlotlyJS, CSV, DataFrames
+
+app = dash(external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"])
+
+df = dataset(DataFrame, "tips")
+
+app.layout = html_div() do
+    html_p("x-axis:"),
+    dcc_checklist(
+        id="x-axis", 
+        options=[Dict("value"=> x, "label"=> x) 
+                 for x in ["smoker", "day", "time", "sex"]],
+        value=["time"], 
+        labelStyle=Dict("display"=> "inline-block")
+    ),
+    html_p("y-axis:"),
+    dcc_radioitems(
+        id="y-axis", 
+        options=[Dict("value"=> x, "label"=> x) 
+                 for x in ["total_bill", "tip", "size"]],
+        value="total_bill", 
+        labelStyle=Dict("display"=> "inline-block")
+    ),
+    dcc_graph(id="box-plot")
+end
+
+callback!(app, Output("box-plot", "figure"), [Input("x-axis", "value"), Input("y-axis", "value")]) do x, y
+    fig = plot(
+        [
+            box(x=df[!, x_col], y=df[!, y])
+            for x_col = x
+        ]
+    )
+    return fig
+end
+run_server(app, "0.0.0.0", 8080)
