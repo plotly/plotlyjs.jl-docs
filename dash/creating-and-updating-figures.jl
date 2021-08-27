dbc299daf337daff3bd7eca416c754f092f8d7a4
Trevor Lyon <trevor.lyon@icloud.com>
2021-08-27 12:20:15 -0300

    dash creating-and-updating-figures

---
+++ dash/creating-and-updating-figures.jl
@@ -0,0 +1,32 @@
+using Dash
+using DashHtmlComponents
+using DashCoreComponents
+using PlotlyJS, CSV, DataFrames
+using JSON
+
+app = dash(external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"])
+
+df = dataset(DataFrame, "tips")
+
+fig = plot(scatter(mode="lines", x=["a","b","c"], y=[1,3,2], 
+title="sample figure", height=325))
+
+app.layout = html_div() do
+    dcc_graph(id = "graph", figure=fig),
+    html_pre(
+        id="structure",
+        style=Dict(
+            "border"=>"thin lightgrey solid", 
+            "overflowY"=>"scroll",
+            "height"=>"275px"
+        )
+    )
+end
+
+# TODO: Can't pretty print json
+callback!(app, Output("structure", "children"), Input("graph", "figure")) do val
+   return json(val)
+end
+
+
+run_server(app, "0.0.0.0", 8080)
