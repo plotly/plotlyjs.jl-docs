927c109b07cea98b275a49debfd7f420e476095a
Trevor Lyon <trevor.lyon@icloud.com>
2021-08-30 11:02:09 -0300

    changes requested

--- dash/histograms.jl
+++ dash/histograms.jl
@@ -11,14 +11,24 @@ df_2007 = df[df.year .== 2007, :]
 app.layout = html_div() do
     dcc_graph(id = "graph"),
     html_p("Mean:"),
-    dcc_slider(id="mean", min=-3, max=3, value=0, 
-               marks=Dict("-3"=> "-3", "3"=>"3")),
+    dcc_slider(
+        id="mean", 
+        min=-3, max=3, value=0, 
+        marks=Dict("-3"=> "-3", "3"=>"3")
+    ),
     html_p("Standard Deviation:"),
-    dcc_slider(id="std", min=1, max=3, value=1, 
-               marks=Dict("1"=> "1", "3"=> "3"))
+    dcc_slider(
+        id="std", 
+        min=1, max=3, value=1, 
+        marks=Dict("1"=> "1", "3"=> "3")
+    )
 end
 
-callback!(app, Output("graph", "figure"), [Input("mean", "value"), Input("std", "value")]) do mean, std
+callback!(
+    app, 
+    Output("graph", "figure"), 
+    [Input("mean", "value"), Input("std", "value")]
+) do mean, std
     data = rand(Normal(mean, std), 500)
     fig = plot(histogram(
         x=data, nbins=30, range_x=[-10,10]
