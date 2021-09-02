6c530140766a0048069d123999679e62c8c4750a
Spencer Lyon <spencerlyon2@gmail.com>
2021-09-02 11:55:22 -0400

    dash/line-and-scatter ready

--- dash/line-and-scatter.jl
+++ dash/line-and-scatter.jl
@@ -3,19 +3,19 @@ using DashHtmlComponents
 using DashCoreComponents
 using PlotlyJS, CSV, DataFrames
 
-app = dash(external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"])
+app = dash(external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"])
 
 df = dataset(DataFrame, "iris")
 
 app.layout = html_div() do
-    dcc_graph(id = "scatter-plot"),
+    dcc_graph(id="scatter-plot"),
     html_p("Petal Width:"),
     dcc_rangeslider(
-        id="rangeslider", 
-        min=0, 
-        max=2.5, 
-        step=0.1, 
-        marks=Dict("0"=>"0", "2.5"=>"2.5"), 
+        id="rangeslider",
+        min=0,
+        max=2.5,
+        step=0.1,
+        marks=Dict("0" => "0", "2.5" => "2.5"),
         value=[0.5, 2]
     )
 end
@@ -25,16 +25,17 @@ callback!(app, Output("scatter-plot", "figure"), Input("rangeslider", "value")) 
     high = val[2]
     mask = df[df.petal_width .> low, :]
     mask = mask[mask.petal_width .< high, :]
-    fig = plot(mask, 
-        kind="scatter", 
-        mode="markers", 
+    fig = plot(mask,
+        kind="scatter",
+        mode="markers",
         x=:sepal_width,
-        y=:sepal_length, 
-        color=:species, 
-        marker_size=:petal_length,
-        marker_sizeref=2*maximum(df.petal_length)/(40^2),
-        marker_sizemode="area",
-        hover_data=[:petal_width]
+        y=:sepal_length,
+        color=:species,
+        marker=attr(
+            size=:petal_length,
+            sizeref=2 * maximum(df.petal_length) / (40^2),
+            sizemode="area",
+        ),
     )
     return fig
 end
