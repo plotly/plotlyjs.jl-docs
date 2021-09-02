24815a51b67ecf0a45ce43b1fa443cf37f2f68d9
Spencer Lyon <spencerlyon2@gmail.com>
2021-09-02 14:29:08 -0400

    use Plot(::NamedTuple) to fix dash/hover-text-and-formatting

--- dash/hover-text-and-formatting.jl
+++ dash/hover-text-and-formatting.jl
@@ -7,41 +7,42 @@ df = dataset(DataFrame, "gapminder")
 oceania = df[df.continent .== "Oceania", :]
 
 default_fig = plot(
-    oceania, 
+    oceania,
     mode="markers+lines",
     hovertemplate=nothing,
-    x=:year, 
-    y=:lifeExp, 
-    color=:country, 
+    x=:year,
+    y=:lifeExp,
+    color=:country,
     title="Hover over points to see the change",
     Layout(hovermode="closest")
 )
 
-app = dash(external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"])
+app = dash(external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"])
 
-app.layout = html_div() do 
+app.layout = html_div() do
     html_p("Hovermode"),
     dcc_radioitems(
         id="hovermode",
-        labelStyle=(display="inline-block",),
+        labelStyle=(display = "inline-block",),
         options=[
-            (label=x, value=x)
+            (label = x, value = x)
             for x in ["x", "x unified", "closest"]
         ],
         value="closest"
     ),
     dcc_graph(id="graph", figure=default_fig)
-    
+
 end
 
 callback!(
     app,
     Output("graph", "figure"),
     Input("hovermode", "value"),
-    State("graph" ,"figure")
+    State("graph", "figure")
 ) do hovermode, fig
-    fig.layout.hovermode = hovermode
-    return fig
+    plt = Plot(fig)
+    relayout!(plt, hovermode=hovermode)
+    plt
 end
 
 run_server(app, "0.0.0.0", 8080)
