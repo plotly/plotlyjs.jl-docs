707e447589faadad54bf35db99d730fc44e0de8e
Spencer Lyon <spencerlyon2@gmail.com>
2021-09-02 12:03:59 -0400

    remove un-needed dataframe stuff in dash/histograms.jl

--- dash/histograms.jl
+++ dash/histograms.jl
@@ -3,30 +3,27 @@ using DashHtmlComponents
 using DashCoreComponents
 using PlotlyJS, CSV, DataFrames
 using Distributions
-app = dash(external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"])
-
-df = dataset(DataFrame, "gapminder")
-df_2007 = df[df.year .== 2007, :]
+app = dash(external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"])
 
 app.layout = html_div() do
-    dcc_graph(id = "graph"),
+    dcc_graph(id="graph"),
     html_p("Mean:"),
     dcc_slider(
-        id="mean", 
-        min=-3, max=3, value=0, 
-        marks=Dict("-3"=> "-3", "3"=>"3")
+        id="mean",
+        min=-3, max=3, value=0,
+        marks=Dict("-3" => "-3", "3" => "3")
     ),
     html_p("Standard Deviation:"),
     dcc_slider(
-        id="std", 
-        min=1, max=3, value=1, 
-        marks=Dict("1"=> "1", "3"=> "3")
+        id="std",
+        min=1, max=3, value=1,
+        marks=Dict("1" => "1", "3" => "3")
     )
 end
 
 callback!(
-    app, 
-    Output("graph", "figure"), 
+    app,
+    Output("graph", "figure"),
     [Input("mean", "value"), Input("std", "value")]
 ) do mean, std
     data = rand(Normal(mean, std), 500)
