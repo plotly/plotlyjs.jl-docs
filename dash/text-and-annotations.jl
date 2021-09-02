e227936578921eeb2d8c111a7bcdd447c8e1f4e5
Spencer Lyon <spencerlyon2@gmail.com>
2021-09-02 15:03:31 -0400

    format

--- dash/text-and-annotations.jl
+++ dash/text-and-annotations.jl
@@ -6,44 +6,38 @@ using PlotlyJS, CSV, DataFrames
 df = dataset(DataFrame, "gapminder")
 df_07 = df[df.year .== 2007, :]
 
-app = dash(external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"])
+app = dash(external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"])
 
-app.layout = html_div() do 
+app.layout = html_div() do
     dcc_graph(id="graph"),
     html_p("Text Position"),
     dcc_radioitems(
         id="pos-x",
-        options=[
-            (label=x, value=x)
-            for x in ["left", "center", "right"]
-        ],
+        options=[(label = x, value = x) for x in ["left", "center", "right"]],
         value="center",
-        labelStyle=(display="inline-block",)
+        labelStyle=(display = "inline-block",)
     ),
     dcc_radioitems(
         id="pos-y",
-        options=[
-            (label=x, value=x)
-            for x in ["top", "bottom"]
-        ],
+        options=[(label = x, value = x) for x in ["top", "bottom"]],
         value="top",
-        labelStyle=(display="inline-block",)
+        labelStyle=(display = "inline-block",)
     )
 end
 
 callback!(
-    app, 
+    app,
     Output("graph", "figure"),
     [Input("pos-x", "value"), Input("pos-y", "value")]
 ) do pos_x, pos_y
-    print(string(pos_y, " " ,pos_x))
+    print(string(pos_y, " ", pos_x))
     fig = plot(
         df_07,
         mode="markers+text",
         x=:gdpPercap, y=:lifeExp,
         text=:country,
         size_max=60,
-        textposition=string(pos_y, " " ,pos_x),
+        textposition=string(pos_y, " ", pos_x),
         Layout(
             xaxis_log=true,
             title="GDP and Life Expectancy, 2007"
