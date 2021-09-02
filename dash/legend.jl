85f2e7ff136bd4778d9d1ec69adc15c784f65415
Spencer Lyon <spencerlyon2@gmail.com>
2021-09-02 12:01:26 -0400

    dash/legend ready

--- dash/legend.jl
+++ dash/legend.jl
@@ -3,34 +3,33 @@ using DashHtmlComponents
 using DashCoreComponents
 using PlotlyJS, CSV, DataFrames
 
-app = dash(external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"])
+app = dash(external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"])
 
-df = dataset(DataFrame, "gapminder")
-df_2007 = df[df.year .== 2007, :]
+df = filter(x -> x.year == 2007, dataset(DataFrame, "gapminder"))
 
 app.layout = html_div() do
-    dcc_graph(id = "graph"),
+    dcc_graph(id="graph"),
     html_p("Legend position"),
     dcc_radioitems(
         id="xanchor",
-        options=[(label="left", value=0), (label="right", value=1)],
+        options=[(label = "left", value = 0), (label = "right", value = 1)],
         value=0,
-        labelStyle=(display= "inline-block",)
+        labelStyle=(display = "inline-block",)
     ),
     dcc_radioitems(
-        id="yanchor", 
-        options=[(label= "top", value= 1), (label= "bottom", value= 0)],
+        id="yanchor",
+        options=[(label = "top", value = 1), (label = "bottom", value = 0)],
         value=1,
-        labelStyle=(display="inline-block",)
+        labelStyle=(display = "inline-block",)
     )
 end
 
 callback!(app, Output("graph", "figure"), [Input("xanchor", "value"), Input("yanchor", "value")]) do pos_x, pos_y
     fig = plot(
         mode="markers",
-        df, x=:gdpPercap, y=:lifeExp, 
-        color=:continent, marker_size=:pop, 
-        marker_sizeref = 2*maximum(df.pop)/(40^2),
+        df, x=:gdpPercap, y=:lifeExp,
+        color=:continent, marker_size=:pop,
+        marker_sizeref=2 * maximum(df.pop) / (40^2),
         marker_sizemode="area",
         size_max=45, log_x=true
     )
