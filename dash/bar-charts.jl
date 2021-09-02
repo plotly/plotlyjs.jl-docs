40ba48d3ae03a6ad5794f94aa4848c3f9bc7a33c
Spencer Lyon <spencerlyon2@gmail.com>
2021-09-02 11:12:59 -0400

    move dash deps to separate repo

--- dash/bar-charts.jl
+++ dash/bar-charts.jl
@@ -3,22 +3,21 @@ using DashHtmlComponents
 using DashCoreComponents
 using PlotlyJS, CSV, DataFrames
 
-app = dash(external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"])
+app = dash(external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"])
 
 df = dataset(DataFrame, "tips")
 days = unique(df.day)
 
 app.layout = html_div() do
-    dcc_dropdown(id="dropdown", options=[(label=x, value=x) for x in days], value=days[1]),
+    dcc_dropdown(id="dropdown", options=[(label = x, value = x) for x in days], value=days[1]),
     dcc_graph(
-        id = "barchart"
+        id="barchart"
     )
 end
 
 callback!(app, Output("barchart", "figure"), Input("dropdown", "value")) do val
-   mask = df[df.day .== val, :]
-   fig = plot(mask, kind="bar", x=:sex, y=:total_bill, color=:smoker, barmode="group")
-   return fig
+    mask = df[df.day .== val, :]
+    plot(mask, kind="bar", x=:sex, y=:total_bill, color=:smoker, barmode="group")
 end
 
 
